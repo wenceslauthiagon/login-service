8410c02c37ee3003dd8a9e242a2a709506848188
wenceslauthiagon <wenceslauncthiago@outlook.com>
2023-03-26 17:39:40 -0300

    including LocalStrategy in auth module.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
